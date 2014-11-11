226873a414ac0d3d0900e407782f6156200b383b
ngnono4air <ngnono@gmail.com>
2014-11-11 15:21:49 +0800

    add post 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ if ($(window).width() <= 1280) {
 
 // Variables
     tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag2       = $('.search'),
+    tag3       = $('.nodejs'),
+    tag4       = $('.java'),
+    tag5       = $('.走着'),
+    tag6       = $('.help');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
